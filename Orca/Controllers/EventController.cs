5aa66d22cf593e53aeb2fd8c95a32620acb1974b
Emeralda Sesari <31917225+EmeraldaSesari@users.noreply.github.com>
2021-03-01 17:51:40 +0000

    Add files via upload

---
+++ Orca/Controllers/EventController.cs
@@ -0,0 +1,48 @@
+using Orca.Services;
+using Microsoft.Extensions.Logging;
+using Orca.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Orca.Controllers
+{
+    // -- /api/events/mock
+    [ApiController]
+    [Route("api/events/mock")]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventAggregator _eventAggregator;
+        public EventController(IEventAggregator eventAggregator)
+        {
+            // Passing event aggregator to event controller.
+            _eventAggregator = eventAggregator;
+        }
+
+        [HttpGet]
+        public StudentEvent GenerateEvent()
+        {
+            StudentEvent testEvent = new StudentEvent
+            {
+                CourseID = "COMP0088", // Course ID Upper case.
+                Timestamp = DateTime.UtcNow,
+                EventType = EventType.Attendance,
+                ActivityType = "Video",
+                ActivityName = "Weekly Lecture",
+                Student = new Student 
+                { 
+                    Email = "zard.best@example.com",
+                    FirstName = "Zard",
+                    LastName = "Best",
+                    ID = "202001991"
+                }
+            };
+
+            _eventAggregator.ProcessEvent(testEvent);
+            // Print event info in console.
+            return testEvent;
+            // Get response on the swagger UI.
+        }
+
+    }
+}
